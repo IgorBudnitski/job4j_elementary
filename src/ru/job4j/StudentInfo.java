bcd44ec582f41bdd52de9b1a7cd0e8ca4bef3bda
igorb <igor.budnitski@gmail.com>
2021-03-29 21:11:30 +0300

    Разбор первой программы. [#366910]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Igor Budnitski");
+        System.out.println("01/02/1988");
+    }
+}
