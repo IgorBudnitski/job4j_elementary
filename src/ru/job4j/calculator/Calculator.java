e63d6a5e9ce1082236fe2c73cf062f55002ef603
igorb <igor.budnitski@gmail.com>
2021-03-29 22:10:46 +0300

    Аргументы [#366902]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -2,6 +2,7 @@ package ru.job4j.calculator;
 
 public class Calculator {
     public static void main(String[] args) {
+/*
         int one = 1;
         int two = 2;
         int onePlusTwo = one + two;
@@ -18,6 +19,14 @@ public class Calculator {
         System.out.println("sixDivTwo = " + sixDivTwo);
         System.out.println("fiveMinusTwo = " + fiveMinusTwo);
         System.out.println("fourTimeTwo = " + fourTimeTwo);
-
+*/
+        int one = 1;
+        int two = 2;
+        int result = one + two;
+        System.out.println(result);
+        one = 10;
+        two = 11;
+        result = one + two;
+        System.out.println(result);
     }
 }
