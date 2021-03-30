c858d82f84b64d02f76cd8d1586fd45e394e8fe8
igorb <igor.budnitski@gmail.com>
2021-03-30 08:49:55 +0300

    Checkstyle [#366906]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idea = "I like Java!!";
+        String idea = "I like Java!!!";
         idea = idea + "But I am a newbie";
         int year = 2021;
         idea = idea + " " + year;
