13db88f18e5737e61e4c3838c2984451c6dc7887
igorb <igor.budnitski@gmail.com>
2021-03-30 08:39:46 +0300

    String [#366901]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+import java.util.Arrays;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        idea = idea + "But I am a newbie";
+        int year = 2021;
+        idea = idea + " " + year;
+        System.out.println(idea);
+    }
+}
