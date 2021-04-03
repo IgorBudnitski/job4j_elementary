389b5016ab2aea39a34a6e725dcc3708c6b86855
igorb <igor.budnitski@gmail.com>
2021-04-03 10:00:02 +0300

    Тернарное сравнение [#366851]

--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -18,9 +18,18 @@ public class MaxTest {
     @Test
     public void whenMax1To1Then1() {
         int left = 1;
-        int right = 2;
+        int right = 1;
         int result = Max.max(left, right);
-        int expected = 2;
+        int expected = 1;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax3To1Then1() {
+        int left = 3;
+        int right = 1;
+        int result = Max.max(left, right);
+        int expected = 3;
         Assert.assertEquals(result, expected);
     }
 }
