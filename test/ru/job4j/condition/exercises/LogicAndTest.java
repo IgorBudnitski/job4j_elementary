651e5e30af97810880eb0444148d625d06c28099
igorb <igor.budnitski@gmail.com>
2021-04-04 09:36:59 +0300

    Проверка двух условий. Логическое И

---
+++ test/ru/job4j/condition/exercises/LogicAndTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition.exercises;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LogicAndTest {
+
+    @Test
+    public void test() {
+        assertFalse(LogicAnd.check(-1));
+        assertFalse(LogicAnd.check(1));
+        assertTrue(LogicAnd.check(2));
+    }
+}
