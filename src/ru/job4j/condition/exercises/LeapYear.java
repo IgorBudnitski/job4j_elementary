99ceb57ddcbaf27023f42e0c08b0aba37b885cbb
igorb <igor.budnitski@gmail.com>
2021-04-04 11:52:08 +0300

    Високосный год.

---
+++ src/ru/job4j/condition/exercises/LeapYear.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition.exercises;
+
+public class LeapYear {
+    public static boolean checkYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+}
