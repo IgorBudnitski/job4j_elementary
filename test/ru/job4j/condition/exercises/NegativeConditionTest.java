047435ef6fca7105189b6a7d70913e453f1fdb11
igorb <igor.budnitski@gmail.com>
2021-04-04 08:57:43 +0300

    Логические выражения

---
+++ test/ru/job4j/condition/exercises/NegativeConditionTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition.exercises;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class NegativeConditionTest {
+
+    @Test
+    public void test() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        NegativeCondition.main(null);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "true" + ln
+                        + "false" + ln
+        ));
+    }
+}
