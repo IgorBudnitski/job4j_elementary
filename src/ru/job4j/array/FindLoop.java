13daaa3c9a6a0a8bc6e958e52b04599811cc8d83
igorb <igor.budnitski@gmail.com>
2021-04-10 07:17:48 +0300

    Поиск индекса в диапазоне. [#366865]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -1,6 +1,23 @@
 package ru.job4j.array;
 
+/*
+В этом задании нужно усовершенствовать классический поиск. Теперь нужно сделать поиск по массиву, но не во всем массиве, а только в указанном диапазоне.
+    data - массив чисел,
+    el - элемент, который нужно найти,
+    start - индекс, с которого начинаем поиск,
+    finish - индекс, которым заканчиваем поиск.
+ */
+
 public class FindLoop {
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        for (int i = start; i <= finish; i++) {
+            if (data[i] == el) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static int indexOf(int[] data, int el) {
         int rst = -1; /* если элемента нет в массиве, то возвращаем -1. */
         for (int index = 0; index < data.length; index++) {
