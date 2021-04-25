45844a3ee3633cbe2ec3b7a7d8da526c3ac9f322
igorb <igor.budnitski@gmail.com>
2021-04-25 07:56:51 +0300

    Расстояние между точками в трехмерном пространстве. [#367048]

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -6,50 +6,30 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PointTest {
-
     @Test
     public void when00to20then2() {
         int expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.3);
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(0, 2);
+        double out = p1.distance(p2);
+        Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
-    public void when02to20then2() {
-        int expected = 3;
-        int x1 = 0;
-        int y1 = 2;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.3);
+    public void when000to222then346() {
+        double expected = 3.46;
+        Point p1 = new Point(0, 0, 0);
+        Point p2 = new Point(2, 2, 2);
+        double out = p1.distance3d(p2);
+        Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
-    public void when22to22then2() {
-        int expected = 0;
-        int x1 = 2;
-        int y1 = 2;
-        int x2 = 2;
-        int y2 = 2;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.3);
+    public void whenn534to1043then1506() {
+        double expected = 15.06;
+        Point p1 = new Point(-5, 3, 4);
+        Point p2 = new Point(10, 4, 3);
+        double out = p1.distance3d(p2);
+        Assert.assertEquals(expected, out, 0.01);
     }
-
-    @Test
-    public void when33to45then2() {
-        double expected = 2.23;
-        int x1 = 3;
-        int y1 = 3;
-        int x2 = 4;
-        int y2 = 5;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.3);
-    }
-
-
 }
