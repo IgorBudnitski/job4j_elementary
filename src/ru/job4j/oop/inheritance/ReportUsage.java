64c8444de08cba787a76e41216c3b32e4e250601
igorb <igor.budnitski@gmail.com>
2021-04-26 21:54:35 +0300

    Аннотация @Override [#367065]

--- src/ru/job4j/oop/inheritance/ReportUsage.java
+++ src/ru/job4j/oop/inheritance/ReportUsage.java
@@ -2,18 +2,17 @@ package ru.job4j.oop.inheritance;
 
 public class ReportUsage {
     public static void main(String[] args) {
-/*
+
         TextReport report = new TextReport();
-        String text = report.generate("Report's name", "Report's body");
+        String text = report.generate("Text Report's name", "Text Report's body");
         System.out.println(text);
-*/
 
         HtmlReport report2 = new HtmlReport();
-        String text2 = report2.generate("Report's name", "Report's body");
+        String text2 = report2.generate("HTML Report's name", "HTML Report's body");
         System.out.println(text2);
 
         JSONReport report3 = new JSONReport();
-        String text3 = report3.generate("Report's name", "Report's body");
+        String text3 = report3.generate("JSON Report's name", "JSON Report's body");
         System.out.println(text3);
     }
 }
